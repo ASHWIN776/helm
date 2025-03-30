103f61ed5d675e70bc1c141289d70d590dd8328f
Ashwin Anil <ashwinrockon123@gmail.com>
2025-03-30 10:51:53 +0900

    config: Project Setup
    - eslint
    - prettier
    - tanstack query, and its eslint plugin

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["expo", "prettier", "plugin:@tanstack/query/recommended"],
+  plugins: ["prettier", "react-native"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};

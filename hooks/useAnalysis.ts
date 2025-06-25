d13c3db1fc279b1bf0a1d8265cf8adb9140556d9
Ashwin Anil <ashwinrockon123@gmail.com>
2025-06-25 22:57:12 +0530

    fix: better and flexible handling for error responses for the useAnalysis hook

--- hooks/useAnalysis.ts
+++ hooks/useAnalysis.ts
@@ -4,17 +4,25 @@ import { BASE_URL } from "@/utils/constants";
 import { authenticatedFetch } from "@/utils/helpers";
 
 const getAnalysis = async (message: string) => {
-  const response = await authenticatedFetch(`${BASE_URL}/api/analysis`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ question: message }),
-  });
-  if (!response.ok) {
+  try {
+    const response = await authenticatedFetch(`${BASE_URL}/api/analysis`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ question: message }),
+    });
+    const data = await response.json();
+
+    if (response.ok || [400, 500].includes(response.status)) {
+      return data;
+    }
+
+    throw new Error("Fetch Error, status code: " + response.status);
+  } catch (error) {
+    console.error("Error fetching analysis:", error);
     throw new Error("Failed to fetch analysis");
   }
-  return response.json();
 };
 
 export const useAnalysis = (message: string) => {

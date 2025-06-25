c4627147476a6e90300c4201c14c6bb414f95f11
Ashwin Anil <ashwinrockon123@gmail.com>
2025-06-25 18:14:26 +0530

    fix: corrected constants for backend urls

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,4 @@
-import { Platform } from "react-native";
-
-export const BASE_URL =
-  Platform.OS === "ios"
-    ? process.env.EXPO_PUBLIC_BACKEND_URL
-    : process.env.EXPO_PUBLIC_BACKEND_URL_ANDROID;
+export const BASE_URL = process.env.EXPO_PUBLIC_BACKEND_URL as string;
 
 export const ADD_BUTTON_ROUTES = ["/", "/transactions"];
 
